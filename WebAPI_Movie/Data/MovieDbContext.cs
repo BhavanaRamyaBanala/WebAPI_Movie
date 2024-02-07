cbdeec503b727e6141ba3d39773da6a078760346
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-07 15:25:28 +0530

    First Commit

---
+++ WebAPI_Movie/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_Movie.Models;
+
+namespace WebAPI_Movie.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPI_Movie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
